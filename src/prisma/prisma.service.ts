dbeca21dff3d9d503471066aea13c6a1ddcf789b
Alexandru Stefanescu <stefanescualexandru1994@gmail.com>
2023-03-30 15:01:54 +0000

    changed with config service to protect private information

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
-
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(private _config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: process.env.DATABASE_URL,
+                    url: _config.get('DATABASE_URL'),
                 }
             }
         })
